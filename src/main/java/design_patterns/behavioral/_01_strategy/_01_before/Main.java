744e77ae69e5a3cc0910e8d46f4e77145d53446b
dylee <leeduyoung2002@gmail.com>
2023-06-28 23:06:36 +0900

    first commit

---
+++ src/main/java/design_patterns/behavioral/_01_strategy/_01_before/Main.java
@@ -0,0 +1,7 @@
+package main.java.design_patterns.behavioral._01_strategy._01_before;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
